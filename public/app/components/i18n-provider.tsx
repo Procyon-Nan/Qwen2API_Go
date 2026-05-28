c7cae0b75f8b5653108f51c1d871be7f0a850119
MapleLeaf <197728340@qq.com>
2026-05-28 15:54:54 +0800

    del: lingma

--- public/app/components/i18n-provider.tsx
+++ public/app/components/i18n-provider.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useSyncExternalStore } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
 
+const subscribe = () => () => {};
+
 export function I18nProvider({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => setMounted(true), []);
+  const mounted = useSyncExternalStore(subscribe, () => true, () => false);
   if (!mounted) return <>{children}</>;
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
